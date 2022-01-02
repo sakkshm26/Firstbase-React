41e15a138b96329850ff8326cec38a2a66bf64b8
shubhambajare <shubhambajare8@gmail.com>
2022-01-02 21:54:07 +0530

    page seven api

--- src/components/Seven/seven.js
+++ src/components/Seven/seven.js
@@ -4,44 +4,49 @@ import logo from '../../assets/firstbase.png';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 
-function Seven() {
+const Seven = () => {
 
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        axios.get('http://3.110.143.87/index.php?r=jprimary/allindustry').then(response => {
-            setData(response.data.industries);
-        }).catch('Error');
-    }, []);
+
+        if (data.length === 0)
+            axios.get('http://3.110.143.87/index.php?r=jprimary/allindustry').then(response => {
+                setData(response.data.industries);
+                console.log(response.data.industries)
+            }).catch('Error');
+    })
 
     return (
         <div className='seven'>
             <div className="container">
                 <img src={logo} />
-                <div className="box">
+                <div class="box">
                     <h4>company details-Delware.llc</h4>
                     <h2>Select an Industry</h2>
                     <h6>What kind of business are you starting?</h6>
-                    <div className="box2">
-                        <div className="box3">
+                    <div class="box2">
+                        <div class="box3">
                             <form>
                                 <label>
                                     Industry<span id="span">*</span>
                                 </label><br />
                                 <input type="text" list="search" />
                                 <datalist id="search">
-                                    {/* {
-                                        data.length ?
-                                            data.map((industry => {
-                                                <option value={industry.name} />
-                                            }))
-                                            :
-                                            <option value='--' />
-                                    } */}
-                                    <option value="B2B software" />
+
+                                    {
+                                        data.map((industry, index) =>
+                                            <option key={index} value={industry.name} />
+                                        )
+                                    }
+
+                                    {/* <option value="B2B software" />
+                                    <option value="B2C software" />
                                     <option value="consultant/contractor" />
                                     <option value="E-commerce" />
-                                    <option value="Education" />
+                                    <option value="Education" /> */}
+
+
                                 </datalist>
 
                             </form>
@@ -51,7 +56,7 @@ function Seven() {
 
 
 
-                    <div className="button1">
+                    <div class="button1">
                         <NavLink to='/five'>&larr;back</NavLink>
                         <NavLink className='next' to='/thirteen'>Next</NavLink>
                     </div>
