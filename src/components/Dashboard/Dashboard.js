cb79ecf4765ea8b37ad8cb78c972f36e00ce1015
udayraj713 <udayraj9923@gmail.com>
2022-01-06 21:38:44 +0530

    dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,17 +3,19 @@ import './Dashboard.css';
 import logo from '../../assets/firstbase.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faQuestion, faCog, faAngleDown, faSignOutAlt, faExclamationCircle, faAngleRight } from '@fortawesome/free-solid-svg-icons';
-import { Navbar, Container, Nav, NavDropdown, FormControl, Button, Offcanvas, Row, Col, Form, Card,Badge } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, FormControl, Button, Offcanvas, Row, Col, Form, Card, Badge } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-
+import payroll from '../../assets/payroll.png';
+import card from '../../assets/card.png';
 function Dashboard() {
     return (
         <div className='dashboard'>
             <nav className='nav-bar'>
                 <div className='top'>
                     <div className='logo'>
-                        <img src={logo} />
+                        <img src={logo}/>
                     </div>
+
                     <div className='nav-links'>
                         <ul>
                             <li>Dashboard</li>
@@ -24,6 +26,7 @@ function Dashboard() {
                             <li>Rewards</li>
                         </ul>
                     </div>
+
                 </div>
                 <div className='bottom'>
                     <ul>
@@ -83,79 +86,85 @@ function Dashboard() {
                 <div className='main-view'>
                     <h3>Good morning,<span>Rohit!</span></h3>
                     <div className='steps'>
-                        <div className='step-1'>
+                        <div className='step-1 p-3'>
                             <div className='step-top'>
                                 <h5>STEP 1</h5>
-                                <p>In Progress</p>
+                                <Badge pill bg="success" text="light">
+                                   Completed
+                                </Badge>
                             </div>
-                            <h4>Company Formation</h4>
+                            <h4 className='mb-2'>Company Registration</h4>
                             <p>2 to 7 business days on average.</p>
                         </div>
-                        <div className='step-2'>
+                        <div className='step-2 p-3'>
                             <div className='step-top'>
                                 <h5>STEP 2</h5>
                                 <Badge pill bg="warning" text="dark">
-    Warning
-  </Badge>
+                                    In Progress
+                                </Badge>
                             </div>
-                            <h4>Post-formation package</h4>
+                            <h4 className='mb-2'>GST, SHopact, Udhyam, aadhar, Startup India</h4>
                             <p>2 to 7 business days on average.</p>
                         </div>
-                        <div className='step-3'>
+                        <div className='step-3 p-3'>
                             <div className='step-top'>
                                 <h5>STEP 3</h5>
-                                <p>In Progress</p>
+                                <Badge pill bg="primary" text="light">
+                                    Pending
+                                </Badge>
                             </div>
-                            <h4>Tax ID (EIN)</h4>
+                            <h4 className='mb-2'>Bank Account Opening</h4>
                             <p>20 to 30 business days once SS-4 is signed.</p>
                         </div>
-                        <div className='step-4'>
+                        <div className='step-4 p-3'>
                             <div className='step-top'>
                                 <h5>STEP 4</h5>
-                                <p>Pending</p>
+                                <Badge pill bg="warning" text="dark">
+                                   In Progress
+                                </Badge>
                             </div>
-                            <h4>BANK ACCOUNT</h4>
+                            <h4 className='mb-2'>Post Information Packages</h4>
                             <p>2 to 7 business days after Tax ID assigned.</p>
                         </div>
                     </div>
-                    <div className='warning'>
+                    {/* <div className='warning'>
                         <FontAwesomeIcon icon={faExclamationCircle} id='excl-icon' />
                         <p>IMPORTANT: Must complete all tasks for timely and accurate processing of your company's EIN and bank account.</p>
-                    </div>
-                    {/* <div className='company-details'>
-                        <div className='box'>
-                            <h4>Responsible Party</h4>
-                            <p>The person responsible for forming Gigmo Solutions Inc.</p>
+                    </div> */}
+                    <div className='company-details mt-4'>
+                        <div className='box p-3'>
+                            <h4>Compliances</h4>
+                            <p>Annual compliance package starts at Rs 20000/annum</p>
                             <div className='task'>
-                                <p>Complete Task</p>
+                                <p>Start Package</p>
                                 <FontAwesomeIcon icon={faAngleRight} />
                             </div>
                         </div>
-                        <div className='box'>
-                            <h4>Business Details</h4>
-                            <p>Provide some additional information about Gigmo Solutions Inc.</p>
+                        <div className='box p-3'>
+                            <h4>Most Important Legal Templates</h4>
+                            <p>- Founders Agreement<br></br>- Emplyment agreement<br></br>- Non disclosure Agreement</p>
                             <div className='task'>
-                                <p>Complete Task</p>
+                                <p>Download Now</p>
                                 <FontAwesomeIcon icon={faAngleRight} />
                             </div>
                         </div>
-                        <div className='box'>
-                            <h4>Board of Directors</h4>
-                            <p>Appoint the officers for forming Gigmo Solutions Inc.</p>
+                        <div className='box p-3'>
+                            <h4>Compliance Calender</h4>
+                            <p>Demo Data Here<br></br><br></br><br></br></p>
                             <div className='task'>
-                                <p>Complete Task</p>
+                                <p>Check Due Dates</p>
                                 <FontAwesomeIcon icon={faAngleRight} />
                             </div>
                         </div>
-                        <div className='box'>
+                        {/* <div className='box p-3'>
                             <h4>Officers</h4>
                             <p>Appoint the officers for forming Gigmo Solutions Inc.</p>
                             <div className='task'>
                                 <p>Complete Task</p>
                                 <FontAwesomeIcon icon={faAngleRight} />
                             </div>
-                        </div>
-                        <div className='box'>
+                        </div> */}
+                        {/* <div className='box'>
                             <h4>Puneet Shareholder Details</h4>
                             <p>Provide some basic information about the individual shareholder.</p>
                             <div className='task'>
@@ -170,9 +179,9 @@ function Dashboard() {
                                 <p>Complete Task</p>
                                 <FontAwesomeIcon icon={faAngleRight} />
                             </div>
-                        </div>
+                        </div> */}
                     </div>
-                    <div className='finish-app'>
+                    {/* <div className='finish-app'>
                         <NavLink to='/payment'><button>Finish Application</button></NavLink>
                     </div> */}
                     <div className='rewards'>
@@ -183,38 +192,39 @@ function Dashboard() {
                         <Row>
                             <Form className='d-flex'>
                                 <Col xs={1} className='search'>
-                                    <button type="button" className="btn btn-secondary" >
+                                    <button type="button" className="btn btn-secondary" style={{borderRadius:'0px'}}>
                                         <svg width="15px" height="15px">
                                             <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
                                         </svg>
                                     </button>
                                 </Col>
                                 <Col xs={5} className='input'>
-                                    <Form.Control placeholder="Search" className='text-dark' />
+                                    <Form.Control placeholder="Search" className='text-dark' style={{borderRadius:'0px'}} />
                                 </Col>
                             </Form>
                         </Row>
                     </Container>
                     <div className='vx-card'>
-                    <div className='cards'>
-                        
-                        <div className='section'>
-                            <h2>Razorpay</h2>
-                            <div className='card-text'>
-                                <h4>Stripe</h4>
-                                <p>The world's most powerful and easy to use APIs</p>
+                        <div className='cards'>
+
+                            <div className='section'>
+                                {/* <h2>Razorpay</h2> */}
+                                <img src={payroll}></img>
+                                <div className='card-text'>
+                                    <h5><b>Razorpay X Payroll</b></h5>
+                                    <p>Up to 1 year free payroll</p>
+                                </div>
+                                <FontAwesomeIcon icon={faAngleRight} />
                             </div>
-                            <FontAwesomeIcon icon={faAngleRight} />
-                        </div>
-                       
-                        <div className='section'>
-                            <h2>deel.</h2>
-                            <div className='card-text'>
-                                <h4>deel</h4>
-                                <p>3 months free for contractors and 20% off employees for 1...</p>
+
+                            <div className='section'>
+                            <img src={card}></img>
+                                <div className='card-text'>
+                                    <h5><b>Razorpay Corporate Card</b></h5>
+                                    <p>Credit limit up to ₹10,00,000</p>
+                                </div>
+                                <FontAwesomeIcon icon={faAngleRight} />
                             </div>
-                            <FontAwesomeIcon icon={faAngleRight} />
-                        </div>
                         </div>
                     </div>
                 </div>
